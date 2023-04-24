32e38b2f582894a9ede936a30c2798ab0b38873a
shaynesoh <shaynesoh@gmail.com>
2023-04-24 12:52:56 +0800

    fixed lint errors

--- src/components/WeatherBackground.js
+++ src/components/WeatherBackground.js
@@ -3,7 +3,7 @@ import Particle from './Particle';
 
 const WeatherBackground = React.memo(({ weather }) => {
 
-  const colors = {
+  const colors = useMemo(() => ({
     Clear: [
       { hex: '#60c3c9' },
       { hex: '#fdb813' },
@@ -60,9 +60,70 @@ const WeatherBackground = React.memo(({ weather }) => {
       { hex: '#ffffff' },
       { hex: '#ffffff' }
     ]
-  };
+  }), []);
+  
 
-  const selectedColors = useMemo(() => colors[weather.main] || colors.default, [weather.main]);
+  // const colors = {
+  //   Clear: [
+  //     { hex: '#60c3c9' },
+  //     { hex: '#fdb813' },
+  //     { hex: '#f27022' },
+  //     { hex: '#e4f6f8' },
+  //     { hex: '#01cafe' }
+  //   ],
+  //   Clouds: [
+  //     { hex: '#a4e8fd' },
+  //     { hex: '#ecf4fc' },
+  //     { hex: '#e3e0eb' },
+  //     { hex: '#c6c7db' },
+  //     { hex: '#26c6f8' }
+  //   ],
+  //   Drizzle: [
+  //     { hex: '#f4f4f4' },
+  //     { hex: '#5c7c85' },
+  //     { hex: '#acd1e4' },
+  //     { hex: '#d5dade' },
+  //     { hex: '#8cb4cc' }
+  //   ],
+  //   Rain: [
+  //     { hex: '#457694' },
+  //     { hex: '#5c759e' },
+  //     { hex: '#96b9cd' },
+  //     { hex: '#468f84' },
+  //     { hex: '#a4bfac' }
+  //   ],
+  //   Thunderstorm: [
+  //     { hex: '#4b4c6a' },
+  //     { hex: '#6c6b8d' },
+  //     { hex: '#343855' },
+  //     { hex: '#717171' },
+  //     { hex: '#5d6d9c' }
+  //   ],
+  //   Snow: [
+  //     { hex: '#dfe8e7' },
+  //     { hex: '#c3d6d4' },
+  //     { hex: '#f0e6e4' },
+  //     { hex: '#d2e3ff' },
+  //     { hex: '#f4f4f4' }
+  //   ],
+  //   Mist: [
+  //     { hex: '#dfe7ea' },
+  //     { hex: '#bfd2d9' },
+  //     { hex: '#a4c0c3' },
+  //     { hex: '#96a7ae' },
+  //     { hex: '#7c959c' }
+  //   ],
+  //   default: [
+  //     { hex: '#ffffff' },
+  //     { hex: '#ffffff' },
+  //     { hex: '#ffffff' },
+  //     { hex: '#ffffff' },
+  //     { hex: '#ffffff' }
+  //   ]
+  // };
+
+  // const selectedColors = useMemo(() => colors[weather.main] || colors.default, [weather.main]);
+  const selectedColors = useMemo(() => colors[weather.main] || colors.default, [colors, weather.main]);
   const canvasRef = useRef(null);
   const pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 
@@ -78,7 +139,7 @@ const WeatherBackground = React.memo(({ weather }) => {
       );
       particles[i] = item;
     }
-  }, [Particle]);  
+  }, []);  
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -89,10 +150,8 @@ const WeatherBackground = React.memo(({ weather }) => {
     canvas.height = window.innerHeight * pixelRatio;
     ctx.scale(pixelRatio, pixelRatio);
 
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-
     function animate() {
-      ctx.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       particles.forEach((item) => {
         item.animate(ctx, canvas.width, canvas.height);
