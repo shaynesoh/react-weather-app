93f374acec0c1ed38ba21bd22fd3976c140dcf29
shaynesoh <shaynesoh@gmail.com>
2023-04-25 11:56:13 +0800

    separated color list from background component

--- src/components/WeatherBackground.js
+++ src/components/WeatherBackground.js
@@ -1,68 +1,10 @@
 import React, { useEffect, useRef, useMemo, useCallback } from 'react';
 import Particle from './Particle';
+import { colorList } from '../data/colorList';
 
 const WeatherBackground = React.memo(({ weather }) => {
 
-  const colors = useMemo(() => ({
-    Clear: [
-      { hex: '#60c3c9' },
-      { hex: '#fdb813' },
-      { hex: '#f27022' },
-      { hex: '#e4f6f8' },
-      { hex: '#01cafe' }
-    ],
-    Clouds: [
-      { hex: '#a4e8fd' },
-      { hex: '#ecf4fc' },
-      { hex: '#e3e0eb' },
-      { hex: '#c6c7db' },
-      { hex: '#26c6f8' }
-    ],
-    Drizzle: [
-      { hex: '#f4f4f4' },
-      { hex: '#5c7c85' },
-      { hex: '#acd1e4' },
-      { hex: '#d5dade' },
-      { hex: '#8cb4cc' }
-    ],
-    Rain: [
-      { hex: '#457694' },
-      { hex: '#5c759e' },
-      { hex: '#96b9cd' },
-      { hex: '#468f84' },
-      { hex: '#a4bfac' }
-    ],
-    Thunderstorm: [
-      { hex: '#4b4c6a' },
-      { hex: '#6c6b8d' },
-      { hex: '#343855' },
-      { hex: '#717171' },
-      { hex: '#5d6d9c' }
-    ],
-    Snow: [
-      { hex: '#dfe8e7' },
-      { hex: '#c3d6d4' },
-      { hex: '#f0e6e4' },
-      { hex: '#d2e3ff' },
-      { hex: '#f4f4f4' }
-    ],
-    Mist: [
-      { hex: '#dfe7ea' },
-      { hex: '#bfd2d9' },
-      { hex: '#a4c0c3' },
-      { hex: '#96a7ae' },
-      { hex: '#7c959c' }
-    ],
-    default: [
-      { hex: '#ffffff' },
-      { hex: '#ffffff' },
-      { hex: '#ffffff' },
-      { hex: '#ffffff' },
-      { hex: '#ffffff' }
-    ]
-  }), []);
-
-  const selectedColors = useMemo(() => colors[weather.main] || colors.default, [colors, weather.main]);
+  const selectedColors = useMemo(() => colorList[weather.main] || colorList.default, [colorList, weather.main]);
   const canvasRef = useRef(null);
   const pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 
@@ -79,7 +21,7 @@ const WeatherBackground = React.memo(({ weather }) => {
       particles[i] = item;
     }
   }, []);
-
+  
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
---
+++ src/data/colorList.js
@@ -0,0 +1,58 @@
+export const colorList = {
+    Clear: [
+        { hex: '#60c3c9' },
+        { hex: '#fdb813' },
+        { hex: '#f27022' },
+        { hex: '#e4f6f8' },
+        { hex: '#01cafe' }
+    ],
+    Clouds: [
+        { hex: '#a4e8fd' },
+        { hex: '#ecf4fc' },
+        { hex: '#e3e0eb' },
+        { hex: '#c6c7db' },
+        { hex: '#26c6f8' }
+    ],
+    Drizzle: [
+        { hex: '#f4f4f4' },
+        { hex: '#5c7c85' },
+        { hex: '#acd1e4' },
+        { hex: '#d5dade' },
+        { hex: '#8cb4cc' }
+    ],
+    Rain: [
+        { hex: '#457694' },
+        { hex: '#5c759e' },
+        { hex: '#96b9cd' },
+        { hex: '#468f84' },
+        { hex: '#a4bfac' }
+    ],
+    Thunderstorm: [
+        { hex: '#4b4c6a' },
+        { hex: '#6c6b8d' },
+        { hex: '#343855' },
+        { hex: '#717171' },
+        { hex: '#5d6d9c' }
+    ],
+    Snow: [
+        { hex: '#dfe8e7' },
+        { hex: '#c3d6d4' },
+        { hex: '#f0e6e4' },
+        { hex: '#d2e3ff' },
+        { hex: '#f4f4f4' }
+    ],
+    Mist: [
+        { hex: '#dfe7ea' },
+        { hex: '#bfd2d9' },
+        { hex: '#a4c0c3' },
+        { hex: '#96a7ae' },
+        { hex: '#7c959c' }
+    ],
+    default: [
+    { hex: '#ffffff' },
+    { hex: '#ffffff' },
+    { hex: '#ffffff' },
+    { hex: '#ffffff' },
+    { hex: '#ffffff' }
+    ]
+};
